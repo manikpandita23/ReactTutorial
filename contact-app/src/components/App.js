ba9d9742dc0b333747fd682e8104f63aefc4e4ad
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-07-07 19:39:11 +0530

    import working js files

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,4 +1,8 @@
+import React from "react";
 import 'C:/Users/manik/OneDrive/Desktop/Github/ReactTutorial/contact-app/src/App.css'; 
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
 function App() {
   return <div>Hello World</div>;
 }
