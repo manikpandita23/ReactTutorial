2742f01846500522599c2bc43bf4cf82da99f740
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-07-08 19:29:26 +0530

    State in class component

--- contact-app/src/components/AddContact.js
+++ contact-app/src/components/AddContact.js
@@ -1,19 +1,40 @@
 import React from "react";
 
 class AddContact extends React.Component{
+    state = {
+        name: "",
+        email: "",
+    };
+
+    add = (e)=> {
+        e.preventDefault();
+        if(this.state.name ==="" && this.state.email ===""){
+            alert("All the fields are mandatory");
+            return;
+        }
+    }
     render (){
         return (
             <div className="UI Main">
                 <h2> AddContact</h2>
-                <form className = "UI form">
+                <form className = "UI form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name</label>
-                        <input type="text" name="name" placeholder="Name" />
+                        <input 
+                        type="text" 
+                        name="name" 
+                        placeholder="Name"
+                        value={this.state.name}
+                        onChange={(e)=> this.setState({name: e.target.value})} />
                         
                     </div>
                     <div className="field">
                         <label>Email</label>
-                        <input type="text" name="email" placeholder="Email" />
+                        <input type="text"
+                         name="email"
+                          placeholder="Email"
+                          value={this.state.email}
+                        onChange={(e)=> this.setState({email: e.target.value})} />
                         
                     </div>
                     <button className="UI Button blue">Add</button>
