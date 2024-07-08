3500e500baa4a10dbdf28ce0e9d4786dcb1291ed
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-07-08 19:20:29 +0530

    useState React Hook

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,28 +1,19 @@
-import React from "react";
+import React, {userState, useState} from "react";
 import 'C:/Users/manik/OneDrive/Desktop/Github/ReactTutorial/contact-app/src/App.css'; 
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
+  const [contacts, setContacts]= useState([]);
+return (
+  <div className="ui container">
+    <Header />
+    <AddContact />
+    <ContactList contacts={contacts}/>
 
-  const contacts = [
-    {
-      id:"1",
-      name:"Manik",
-      email:"manik@gmail.com",
-    },
-    {
-      id:"2",
-      name: "abc",
-      email:"abc@gmail.com",
-    }
-  ];
-  return <div>
-    <Header/>
-    <AddContact/>
-    <ContactList contacts = {contacts}/>
-  </div>;
+  </div>
+  );
 }
 
 export default App;
