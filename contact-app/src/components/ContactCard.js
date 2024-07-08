558e5a6b2748a22ef6a8e312e96fed3559be91cb
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-07-08 19:15:58 +0530

    import image class

--- contact-app/src/components/ContactCard.js
+++ contact-app/src/components/ContactCard.js
@@ -1,16 +1,17 @@
 import React from "react";
+import userImage from "../images/user.png";
 
-const ContactCard = (props)=>{
-    const {id, name, email} = props.contact;
-    return(
-        <div className="item">
-                <div className="content">
-                  <div className="header">{name}</div>
-                  <div> {email}</div>  
-                </div>
-                <i className="Trash alternate outline icon"></i>
-                style = {{color:"red", marginTop:"7px"}}
+const ContactCard = (props) => {
+    const { id, name, email } = props.contact;
+    return (
+        <div className="item" style={{ color: "red", marginTop: "7px" }}>
+            <img className="ui avatar image" src={userImage} alt="user" />
+            <div className="content">
+                <div className="header">{name}</div>
+                <div>{email}</div>
             </div>
+        </div>
     );
 };
+
 export default ContactCard;
