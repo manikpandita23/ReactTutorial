b2b7f83436e7d8f4482f89b850c5788011a88511
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-07-11 17:43:44 +0530

    Updated business logic for models and routes (user/users.js)

--- Auction/backend/models/user.js
+++ Auction/backend/models/user.js
@@ -1,38 +1,40 @@
-const mongoose = require ("mongoose");
-const jwt = require ("jsonwebtoken");
-const Joi = require ("joi");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
 
-const userSchema = new mongoose.Schema ({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-    address: {type: String, required: true},
-    date: {type: String, required: true},
-    minAmount: {type: String, required: true},
-    maxAmount: {type: String, required: true},
-
+const userSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    address: { type: String, required: true },
+    date: { type: Date, required: true },  // Corrected to Date type
+    minAmount: { type: Number, required: true },  // Corrected to Number type
+    maxAmount: { type: Number, required: true },  // Corrected to Number type
 });
 
 userSchema.methods.generateAuthToken = function () {
-    const token = jwt.sign({_id: this._id}, process.env.JWTPRIVATEKEY, {
+    const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
         expiresIn: "5d",
     });
     return token;
 };
 
-const validate = (data)=> {
+const User = mongoose.model("User", userSchema);
+
+const validate = (data) => {
     const schema = Joi.object({
         firstName: Joi.string().required().label("First Name"),
         lastName: Joi.string().required().label("Last Name"),
         email: Joi.string().email().required().label("Email"),
         password: passwordComplexity().required().label("Password"),
         address: Joi.string().required().label("Address"),
-        date: Joi.string().required().label("Date"),
-        minAmount: Joi.string().required().label("Min Amount"),
-        maxAmount: Joi.string().required().label("Max Amount"),
+        date: Joi.date().required().label("Date"),  // Corrected to Joi.date()
+        minAmount: Joi.number().required().label("Min Amount"),  // Corrected to Joi.number()
+        maxAmount: Joi.number().required().label("Max Amount"),  // Corrected to Joi.number()
     });
     return schema.validate(data);
-}
-module.export = {User, validate};
+};
+
+module.exports = { User, validate };  // Corrected export statement
